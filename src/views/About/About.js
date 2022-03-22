515f58fb130c9f360ce4bf0280594dfee4a1f8ea
Ray Weitzenberg <ray@neoneon.one>
2022-03-22 15:33:13 -0400

    about page writing/editing

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -12,23 +12,28 @@ let About = (props) => {
         <h1>Ray Weitzenberg</h1>
         <h3>Artist and Technologist inducing delight online and off</h3>
         <p>
-          I'm a visual artist and technologist based out of Brooklyn. In 1994,
-          via the mechanism of photography, I discovered a profound appreciation
-          for light. I've spent the last 24 years collecting and projecting that
-          light, in part shaping that sensibility via the lens of a BFA at
-          Parsons.
+          I'm a visual artist and technologist based in Brooklyn. In 1994, via
+          the mechanism of photography, I discovered a profound appreciation for
+          light. I've spent the last 24 years collecting and projecting that
+          light, in part having shaped that sensibility via the lens of Parsons
+          School of Design.
         </p>
         <p>
-          Some recent work is actualized in AR/VR experiences, both mobile
-          and immersive. Also under active development is a storytelling
-          platform spanning simultaneous digital and physical means of consumption.
+          As much as I am enamored with art, I focus my attention equally on the
+          digital side of the equation. It's when the two passions intersect
+          that monkeys start to dance.
         </p>
         <p>
-          These are often crafted with bespoke components sometimes operating
-          outside my control, in a black box system. Such a system takes input —
-          which can come from any one or more sources — then processes it via
-          loosely coupled constraints, resulting in imagery composed somewhat
-          randomly, bordering on autonomy.
+          Under active development is a storytelling platform spanning
+          simultaneous digital and physical means of interaction. Other recent
+          work is actualized in AR/VR experiences, both mobile and immersive.
+        </p>
+        <p>
+          Elements of these are often crafted with bespoke apps, sometimes
+          operating outside my control ... in a black box system. The systems
+          take input — which can come from any one or more sources — then
+          processes it via loosely coupled constraints, resulting in imagery
+          composed somewhat randomly, bordering on autonomy.
         </p>
       </section>
     </>
