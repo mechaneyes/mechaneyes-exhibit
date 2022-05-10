b620b6635b66b284669e158b4bc23cb980923c2d
Ray Weitzenberg <ray@neoneon.one>
2022-05-10 17:19:20 -0400

    modal js + info cards' html & css + intro styling
    + modal js refactoring .. can be tightened further
    + cleaned up info card html and styled them
    + intro modal styling

--- src/views/MapOne/modals.js
+++ src/views/MapOne/modals.js
@@ -2,101 +2,75 @@ import mapboxgl from "!mapbox-gl";
 /* eslint import/no-webpack-loader-syntax: off */
 
 export const modals = (map) => {
-  // ————————————————————————————————————o————————————————————————————————————o Mechaneyes + About Modals -->
-  // ———————————————————————————————————— Mechaneyes + About Modals —>
-  // Added as cards glued in place via mountains.geojson
-  //
   let staticCards;
+  let infoCards;
+
   fetch("/data/mountains.geojson")
     .then((res) => res.json())
     .then((result) => {
       for (const feature of result.features) {
+
+        // ————————————————————————————————————o————————————————————————————————————o Intro + About Modals -->
+        // ———————————————————————————————————— Intro + About Modals —>
+        // Added as cards glued in place via mountains.geojson
+        //
         if (feature.properties.static) {
           let htmlFile = feature.properties.htmlFile;
           let staticClass = feature.properties.staticClass;
           const el = document.createElement("div");
           el.className = `static-card static-card ${staticClass}`;
 
-          // ———————————————————————————————————— Fetch Mech/About HTML —>
+          // ———————————————————————————————————— Fetch Intro/About HTML —>
           fetch(`/projects/projects/${htmlFile}.html`)
-          .then((response) => response.text())
-          .then((html) => {
-            // console.log(html);
-            el.innerHTML = html;
-          })
-          .catch((err) => {
-            console.log("not so fetchy");
-          });
+            .then((response) => response.text())
+            .then((html) => {
+              // console.log(html);
+              el.innerHTML = html;
+            })
+            .catch((err) => {
+              console.log("not so static fetchy");
+            });
 
           new mapboxgl.Marker(el)
             .setLngLat(feature.geometry.coordinates)
             .addTo(map);
+        } else {
+          // ————————————————————————————————————o————————————————————————————————————o Category Info Modals -->
+          // ———————————————————————————————————— Category Info Modals —>
+          // Added as markers glued in place via mountains.geojson
+          //
+          let infoFile = feature.properties.infoFile;
+          const em = document.createElement("div");
+          em.className = `info-card info-card--hidden ${infoFile}`;
+
+          // ———————————————————————————————————— Fetch Info/About HTML —>
+          fetch(`/info/${infoFile}.html`)
+            .then((response) => response.text())
+            .then((html) => {
+              // console.log(html);
+              em.innerHTML = html;
+            })
+            .catch((err) => {
+              console.log("not so info fetchy");
+            });
+
+          if (feature.properties.info == true) {
+            new mapboxgl.Marker(em)
+              .setLngLat(feature.geometry.coordinates)
+              .addTo(map);
+          }
         }
       }
     })
+
     .then(() => {
       staticCards = document.querySelectorAll(".static-card");
+      infoCards = document.querySelectorAll(".info-card");
 
       map.on("movestart", () => {
         for (const card of staticCards) {
           card.classList.add("static-card--hidden");
         }
-      });
-
-      map.on("moveend", () => {
-        let zoomLevel = map.getZoom();
-        // console.log("zoomLevel", zoomLevel);
-
-        for (const card of staticCards) {
-          if (zoomLevel >= 13.4) {
-            card.style.display = "block";
-            card.classList.remove("static-card--hidden");
-          } else {
-            card.style.display = "none";
-          }
-        }
-      });
-    });
-
-  // ————————————————————————————————————o————————————————————————————————————o Fetch Test -->
-  // ———————————————————————————————————— Fetch Test —>
-  // fetch("/projects/projects/intro.html")
-  //   .then((response) => response.text())
-  //   .then((html) => {
-  //     console.log(html);
-  //     document.querySelector(
-  //       ".mapboxgl-marker.static-card--intro"
-  //     ).innerHTML = html;
-  //   })
-  //   .catch((err) => {
-  //     console.log("not so fetchy");
-  //   });
-
-  // ————————————————————————————————————o————————————————————————————————————o Project Info Modals -->
-  // ———————————————————————————————————— Project Info Modals —>
-  // Added as markers glued in place via mountains.geojson
-  //
-  let infoCards;
-  fetch("/data/mountains.geojson")
-    .then((res) => res.json())
-    .then((result) => {
-      for (const feature of result.features) {
-        let infoFile = feature.properties.infoFile;
-        const el = document.createElement("div");
-        el.className = `info-card info-card--hidden ${infoFile}`;
-        el.innerHTML = `<object class="info-card__object" type="text/html" data="/info/${infoFile}.html"></object>`;
-
-        if (feature.properties.info == true) {
-          new mapboxgl.Marker(el)
-            .setLngLat(feature.geometry.coordinates)
-            .addTo(map);
-        }
-      }
-    })
-    .then(() => {
-      infoCards = document.querySelectorAll(".info-card");
-
-      map.on("movestart", () => {
         for (const card of infoCards) {
           card.classList.add("info-card--hidden");
         }
@@ -106,8 +80,17 @@ export const modals = (map) => {
         let zoomLevel = map.getZoom();
         console.log("zoomLevel", zoomLevel);
 
-        for (const card of infoCards) {
+        for (const card of staticCards) {
           if (zoomLevel >= 13.4) {
+            card.style.display = "block";
+            card.classList.remove("static-card--hidden");
+          } else {
+            card.style.display = "none";
+          }
+        }
+
+        for (const card of infoCards) {
+          if (zoomLevel >= 13) {
             card.style.display = "block";
             card.classList.remove("info-card--hidden");
           } else {
