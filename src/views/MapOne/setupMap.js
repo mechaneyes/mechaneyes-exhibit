7d6b43c01186ad6ba04eba1df99e9b8943f4c6c8
Ray Weitzenberg <ray@neoneon.one>
2022-05-21 22:14:30 -0400

    killed clustering until better implementation

--- src/views/MapOne/setupMap.js
+++ src/views/MapOne/setupMap.js
@@ -90,29 +90,29 @@ export const setupMap = (map, geoFile) => {
   // ————————————————————————————————————o————————————————————————————————————o Cluster Circles -->
   // ———————————————————————————————————— Cluster Circles —>
   //
-  map.addLayer({
-    id: "clusters",
-    type: "circle",
-    source: "mountains",
-    filter: ["has", "point_count"],
-    paint: {
-      // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-      // with three steps to implement three types of circles:
-      //   * Blue, 20px circles when point count is less than 100
-      //   * Yellow, 30px circles when point count is between 100 and 750
-      //   * Pink, 40px circles when point count is greater than or equal to 750
-      "circle-color": [
-        "step",
-        ["get", "point_count"],
-        "#51bbd6",
-        100,
-        "#f1f075",
-        750,
-        "#f28cb1",
-      ],
-      "circle-radius": 70,
-    },
-  });
+  // map.addLayer({
+  //   id: "clusters",
+  //   type: "circle",
+  //   source: "mountains",
+  //   filter: ["has", "point_count"],
+  //   paint: {
+  //     // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+  //     // with three steps to implement three types of circles:
+  //     //   * Blue, 20px circles when point count is less than 100
+  //     //   * Yellow, 30px circles when point count is between 100 and 750
+  //     //   * Pink, 40px circles when point count is greater than or equal to 750
+  //     "circle-color": [
+  //       "step",
+  //       ["get", "point_count"],
+  //       "#51bbd6",
+  //       100,
+  //       "#f1f075",
+  //       750,
+  //       "#f28cb1",
+  //     ],
+  //     "circle-radius": 70,
+  //   },
+  // });
 
   // ———————————————————————————————————— Popup on Label Click —>
   // Popups are the project pages/files
