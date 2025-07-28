a6b6c2e5470a114083dc1b9b7b6c3f27a7c5429d
Ray Weitzenberg <ray@mechaneyes.com>
2025-07-28 17:01:52 -0400

    pager

--- src/views/MapOne/modals.js
+++ src/views/MapOne/modals.js
@@ -88,7 +88,7 @@ export const modals = (map) => {
 
   // Function to open modal programmatically
   const openModal = () => {
-    let htmlFile = "stereoh-design"; // Temporarily hardcoded for dev
+    let htmlFile = "pager"; // Temporarily hardcoded for dev
 
     // Remove any existing popup
     if (currentPopup) {
